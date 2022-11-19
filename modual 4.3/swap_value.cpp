1a9b2195a9d5a175c48c578aa9ad07d3328c3f5f
nileshdemo <sentanilesh1234@gmail.com>
2022-11-19 08:38:51 +0530

    add file

---
+++ modual 4.3/swap_value.cpp
@@ -0,0 +1,27 @@
+//  Write a program of to swap the two values using template
+#include<iostream>
+using namespace std;
+template<class T>
+T swap(T a,T b){
+
+   
+    T temp;
+    temp=a;
+    a=b;
+    b=temp;
+
+    cout<<"After swap "<<a<<" "<<b<<endl;
+    
+}
+int main(){
+     int x=10,y=20;
+    double a1=20.5,b1=10.5;
+    cout<<"Before swap "<<x<<" "<<y<<endl;
+    cout<<"Before swap "<<a1<<" "<<b1<<endl;
+    
+    swap(10,20);
+    swap(20.5,10.5);
+   
+
+    
+}
